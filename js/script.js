3f0edd82cdca8a32510fa9549aa08a90b3f57254
Kim Do Hee <95347166+doright84@users.noreply.github.com>
2022-01-24 17:38:13 +0900

    소울 이벤트 페이지 수정

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
     AOS.init();
-    // 안내창 닫기
-    $('.modal').click(function () {
-        $('.modal').fadeOut();
-    });
+   
     // ---gotop----
     $('.gotop').click(function (e) {
         e.preventDefault();
